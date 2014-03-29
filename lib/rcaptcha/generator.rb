424711bb3f5dd7f08185c238a950a39fcadab6d8
Kyle Dayton <kyle@graphicflash.com>
2014-03-29 03:26:39 -0400

    Added RMagick generator

--- lib/rcaptcha/generator.rb
+++ lib/rcaptcha/generator.rb
@@ -1,20 +1,52 @@
 module RCaptcha
 
   class Generator
-    attr_reader :captcha_text
-
-    # Base class for all generators
-    # Expects text to include in the CAPTCHA image
-    # @param [String] captcha_text the text of the generated CAPTCHA image
-    def initialize(captcha_text)
-      @captcha_text = captcha_text
-    end
 
     # Generates the CAPTCHA image with +captcha_text+
-    # @return [Bytes] the bytes of the generated image
-    def generate
-      raise NotImplementedError
+    # @param [String] captcha_text the text to include in the CAPTCHA image
+    # @return [String] the bytes of the generated image
+    def self.generate(captcha_text, width = 100, height = 32)
+      image = create_image(width, height)
+      draw_text!(captcha_text, image)
+
+      image = image.wave *random_wave_distortion
+      image = image.implode random_implode_distortion
+
+      image.to_blob
     end
+
+    private
+
+    def self.create_image(width, height)
+      image = Magick::Image.new(width, height)
+      image.format = "jpg"
+      image.gravity = Magick::CenterGravity
+      image.background_color = 'white'
+
+      image
+    end
+
+    def self.draw_text!(text, image)
+      draw = Magick::Draw.new
+
+      draw.annotate(image, image.columns, image.rows, 0, 0, text) {
+        self.gravity = Magick::CenterGravity
+        self.pointsize = 22
+        self.fill = 'darkblue'
+        self.stroke = 'transparent'
+      }
+
+      nil
+    end
+
+    def self.random_wave_distortion
+      [4 + rand(2), 30 + rand(20)]
+    end
+
+    def self.random_implode_distortion
+      (2 + rand(2)) / 10.0
+    end
+
   end
   
 end
